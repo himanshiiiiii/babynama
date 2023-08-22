eb138744d69f7ec41f48bb26a5245f1a843e271d
himanshiiiiii <himanshitripathi14@gmail.com>
2023-08-22 22:03:36 +0530

    done2

--- lib/payment/payment.dart
+++ lib/payment/payment.dart
@@ -46,36 +46,56 @@ class _PaymentState extends State<Payment> {
           ),
           Center(
             child: Container(
-              child: LottieBuilder.asset("lottie/babycare2.json",repeat: false,),
+              child: LottieBuilder.asset("lottie/babycare2.json",repeat: false,height: 150,),
             ),
           ),
           Column(
             children: [
-              Text("Payment Sucessful",style: GoogleFonts.poppins(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),),
-
               Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Text("   Thank you for choosing BabyCare \nto accompany you on this incredible \n        adventure of parenthood. \n   We're committed to making this \n  journey as smooth and joyful as \n        possible for you and your \n                   precious one.",
-                  style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Color(0xffcbd5e1)),),
+                padding: const EdgeInsets.only(left: 20.0,right: 20,bottom: 20),
+                child: Text("Payment Sucessful",style: GoogleFonts.poppins(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.white),),
+              ),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Thank you for choosing BabyCare",
+                    style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Color(0xffcbd5e1)),),
+                  Text("to accompany you on this incredible",
+                    style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Color(0xffcbd5e1)),),
+                  Text("adventure of parenthood.",
+                    style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Color(0xffcbd5e1)),),
+                  Text("We're committed to making this",
+                    style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Color(0xffcbd5e1)),),
+                  Text("journey as smooth and joyful as",
+                    style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Color(0xffcbd5e1)),),
+                  Text("possible for you and your",
+                    style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Color(0xffcbd5e1)),),
+                  Text("precious one.",
+                    style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Color(0xffcbd5e1)),),
+                ],
               ),
               GestureDetector(
                 onTap: (){
                   Navigator.push(context,MaterialPageRoute(builder: (context)=>MyHomePage()));
                 },
-                child: Container(
-                  height: 50,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xff7e2cee),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    height: 50,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xff7e2cee),
+                    ),
+                    child: Center(child: Text("Back to Home",style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Color(0xffcbd5e1)),)),
                   ),
-                  child: Center(child: Text("Back to Home",style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Color(0xffcbd5e1)),)),
                 ),
               )
 
             ],
           ),
-          SizedBox(height: 50,)
+
 
         ],
       ),
